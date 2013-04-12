c03f188075b3293dc9cbcc2d33fa02d998b361ee
Alexey Sokolov <scream3r.org@gmail.com>
2013-04-12 09:08:06 +0300

    Cleared SerialPortList class (deleting of unused imports)

--- src/java/jssc/SerialPortList.java
+++ src/java/jssc/SerialPortList.java
@@ -24,11 +24,7 @@
  */
 package jssc;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.TreeSet;
