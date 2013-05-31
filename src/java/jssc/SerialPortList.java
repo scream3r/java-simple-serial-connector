d57c122e2f09ae1f754c7a6ffbd6281a475ebba5
Alexey Sokolov <scream3r.org@gmail.com>
2013-05-31 14:27:49 +0300

    Added ttyO to Linux RegExp for finding serial ports provided by Linux OMAP serial driver

--- src/java/jssc/SerialPortList.java
+++ src/java/jssc/SerialPortList.java
@@ -43,7 +43,7 @@ public class SerialPortList {
         serialInterface = new SerialNativeInterface();
         switch (SerialNativeInterface.getOsType()) {
             case SerialNativeInterface.OS_LINUX: {
-                PORTNAMES_REGEXP = Pattern.compile("(ttyS|ttyUSB|ttyACM|ttyAMA|rfcomm)[0-9]{1,3}");
+                PORTNAMES_REGEXP = Pattern.compile("(ttyS|ttyUSB|ttyACM|ttyAMA|rfcomm|ttyO)[0-9]{1,3}");
                 PORTNAMES_PATH = "/dev/";
                 break;
             }
