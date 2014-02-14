6c55bb04cf6271ec89d5878ef945c1c22da49ffa
gohai <gottfried.haider@gmail.com>
2014-02-14 12:22:46 -0800

    Don't open ports for getPortNames()
    Opening certain devices, such as serial endpoints over Bluetooth, can take a long time before they fail. Moreover, it's better to show devices even when the user currently doesn't have permissions to access them - else one might think of a problem with the device or driver, when a "sudo chmod 666" would do the trick.

--- src/java/jssc/SerialPortList.java
+++ src/java/jssc/SerialPortList.java
@@ -330,6 +330,7 @@ public class SerialPortList {
                     String fileName = file.getName();
                     if(!file.isDirectory() && !file.isFile() && pattern.matcher(fileName).find()){
                         String portName = searchPath + fileName;
+                        /*
                         long portHandle = serialInterface.openPort(portName, false);//Open port without TIOCEXCL
                         if(portHandle < 0 && portHandle != SerialNativeInterface.ERR_PORT_BUSY){
                             continue;
@@ -337,6 +338,7 @@ public class SerialPortList {
                         else if(portHandle != SerialNativeInterface.ERR_PORT_BUSY) {
                             serialInterface.closePort(portHandle);
                         }
+                        */
                         portsTree.add(portName);
                     }
                 }
