4b316077c7f054bb76629e0addfb4e359cf0427d
gohai <gottfried.haider@gmail.com>
2018-03-15 18:53:30 -0700

    Add support for Raspbian (Raspberry Pi Debian) custom serial port names

--- src/java/jssc/SerialPortList.java
+++ src/java/jssc/SerialPortList.java
@@ -47,7 +47,7 @@ public class SerialPortList {
         serialInterface = new SerialNativeInterface();
         switch (SerialNativeInterface.getOsType()) {
             case SerialNativeInterface.OS_LINUX: {
-                PORTNAMES_REGEXP = Pattern.compile("(ttyS|ttyUSB|ttyACM|ttyAMA|rfcomm|ttyO)[0-9]{1,3}");
+                PORTNAMES_REGEXP = Pattern.compile("(serial|ttyS|ttyUSB|ttyACM|ttyAMA|rfcomm|ttyO)[0-9]{1,3}");
                 PORTNAMES_PATH = "/dev/";
                 break;
             }
